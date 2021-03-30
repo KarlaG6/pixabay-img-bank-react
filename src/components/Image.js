f773757455cfeeba8915fc01047c0b2059277ae5
karlaG6 <karlaisabelgt@gmail.com>
2021-03-30 12:54:18 -0500

    mostrando images y links para alta resolucion

--- src/components/Image.js
+++ src/components/Image.js
@@ -7,7 +7,19 @@ const Image = ({image}) => {
         <div className="col-12 col-sm-6 col-md-4 col-lg-3">
             <div className="card">
                 <img src={previewURL} alt={tags} className="card-img-top mb-4" />
+                
+                <div className="card-body">
+                    <p className="card-text">{likes} Me gusta</p>
+                    <p className="card-text">{views} Vistas</p>
+                </div>
+
+                <div className="card-footer">
+                    {/* Siempre que uses --target-- por seguridad agrega --rel-- */}
+                    <a href={largeImageURL} target="_blank" rel="noopener noreferrer" 
+                        className="btn btn-primary btn-block">Ver Imagen</a>
+                </div>
             </div>
+
         </div>
     );
 }
